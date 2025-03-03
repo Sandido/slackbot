1d59dfed70915405378739b622bd995ec292d3d7
Adam Sandor <adamsandor930@gmail.com>
2025-03-03 10:56:13 -0500

    Update app.py

--- app.py
+++ app.py
@@ -11,10 +11,6 @@ from models import QueryRequest
 env_path = Path('.') / '.env'
 load_dotenv(dotenv_path=env_path)
 
-# TODO app is not returning a good challenge to the slack api challenege. 
-# challenge is to /slack/events, so the last thing to try is ensure this code
-# is run on app startup. Renamed it to try and get picked up 
-# automatically as the startup file. 
 app = Flask(__name__)
 slack_event_adapter = SlackEventAdapter(os.environ['SIGNING_SECRET'], '/slack/events', app)
 
