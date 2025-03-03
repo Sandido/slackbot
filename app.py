16f9fa074cb76c1fec1a03796b73ae5e39516463
Adam Sandor <adamsandor930@gmail.com>
2025-03-03 11:08:40 -0500

    Update app.py

--- app.py
+++ app.py
@@ -18,15 +18,6 @@ client = slack.WebClient(token=os.environ['SLACK_BOT_TOKEN'])
 # client.chat_postMessage(channel='#new-channel', text="Hello World 2!")
 BOT_ID = client.api_call("auth.test")['user_id']
 
-# don't need it, used earlier for testing purposes
-@slack_event_adapter.on('message')
-def message(payload):
-    print(payload)
-    event = payload.get('event', {})
-    channel_id = event.get('channel')
-    user_id = event.get('user')
-    text = event.get('text')
-
 # check if bot is running
 @app.route("/")
 def index():
@@ -95,3 +86,13 @@ def translate_message():
 
 if __name__ == "__main__":
     app.run(debug=True, port=5000)
+
+# don't need below, used earlier for testing purposes, keeping for reference in case I want to expand this 
+# in a particular channel just for the translate bot where it replies more often.
+# @slack_event_adapter.on('message')
+# def message(payload):
+#    print(payload)
+#    event = payload.get('event', {})
+#    channel_id = event.get('channel')
+#    user_id = event.get('user')
+#    text = event.get('text')
